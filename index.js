5df808df1d4c9d872080c5d548b7dcae1c3cae59
aramis48 <33667751+aramis48@users.noreply.github.com>
2017-12-04 13:50:43 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+//below will be a function for subtracting
+function subtract(num1,num2){
+  return num1-num2;
+}
+
+//below will be a function for multiplication
+function multiply(num1,num2){
+  return num1*num2;
+}
+
+function divide(num1,num2){
+  return num1/num2
+}
+
+function toCelsius(fahrenheit){
+  return colsole.log(fahrenheit -32 * 5/9);
+}
+
+
+subtract(100, 50);
+mulyiply(5, 10);
+divide(100, 10)
+toCelcius(32);
+// F - 32 *5/9
